6d11b40632439c9c88d961ae62531ee060bd0a59
cliffordh <cliff@phonetransact.com>
2017-11-10 15:03:22 -0600

    Very basic example fetching orderbook and listening to websocket diff-orders channel.

---
+++ src/main/java/sample/WSClient.java
@@ -0,0 +1,58 @@
+package sample;
+
+import java.io.IOException;
+import java.net.URI;
+import javax.websocket.ClientEndpoint;
+import javax.websocket.ContainerProvider;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+@ClientEndpoint
+public class WSClient {
+    private final String uri="wss://ws.bitso.com";
+    private Session session;
+
+   public static void main(String[] args) {
+       WSClient w=new WSClient();
+       w.connect();
+   }
+
+    public void connect() {
+
+        try{
+            WebSocketContainer container=ContainerProvider.
+                    getWebSocketContainer();
+            container.connectToServer(this, new URI(uri));
+            this.sendMessage("");
+
+        }catch(Exception ex){
+            System.out.println(ex);
+
+        }
+
+    }
+
+    @OnOpen
+    public void onOpen(Session session){
+      System.out.println("Session opened");
+       this.session=session;
+        this.sendMessage("{\"action\":\"subscribe\", \"book\":\"btc_mxn\",\"type\":\"diff-orders\"}");
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session){
+        System.out.println(message);
+    }
+
+    public void sendMessage(String message){
+        try {
+            session.getBasicRemote().sendText(message);
+            System.out.println(message);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+}
+
